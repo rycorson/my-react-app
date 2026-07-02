4331969208a3df692999d7cc2780ac693c50ea18
Ryan Corson <ryan.corson88@gmail.com>
2026-07-01 23:07:21 -0500

    Create Student component, add props. Create UserGreeting component, add conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ import Card from './Card.jsx';
 import Food from './Food.jsx'
 import Footer from './Footer.jsx'
 import Header from './Header.jsx'
+import Student from './Student.jsx'
+import UserGreeting from './UserGreeting.jsx';
 
 function App() {
   return (
     <>
       <Header />
+      <UserGreeting isLoggedIn={true} username="Ryan"/>
       <Card />
+      <Student name="Spongebob" age={30} isStudent={true} /> 
+      <Student name="Patrick" age={42} isStudent={false} />
+      <Student name="Squidward" age={50} isStudent={false} />
+      <Student name="Sandy" age={27} isStudent={true} />
+      <Student name="Larry" />
       <Button />
       <Food />
       <Footer />
---
+++ src/Student.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+function Student({name = "Guest", age = 0, isStudent = false}) {
+    return (
+        <div className="student">
+            <p>Name: {name}</p>
+            <p>Age: {age}</p>
+            <p>Student: {isStudent ? "Yes" : "No"}</p>
+        </div>
+    );
+}
+Student.PropTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool
+}
+export default Student;
