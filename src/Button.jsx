4d2541fc6c7841d30af60a6b27ba4107ca564d68
Ryan Corson <ryan.corson88@gmail.com>
2026-07-02 12:46:11 -0500

    Add onClick event handlers to Button.jsx

--- src/Button.jsx
+++ src/Button.jsx
@@ -11,7 +11,27 @@ function Button() {
         cursor: "pointer"
     }
 
-    return(<button style={styles}>Click Me</button>);
+    //const handleClick = () => console.log("ouch!");
+    //const handleClick2 = (name) => console.log(`${name} stop clicking me!`);
+
+    /*let count = 0;
+
+    const handleClick = (name) => {
+        if (count < 3) {
+            count++;
+            console.log(`${name} you clicked me ${count} time/s`);
+        }
+        else {
+            console.log(`${name} stop clicking me!`);
+        }
+    };
+
+    return(<button onClick={() => handleClick("Ryan")} style={styles}>Click Me</button>);*/
+
+    //const handleClick = (e) => console.log(e);
+    const handleClick = (e) => e.target.textContent = "OUCH! 🤕";
+
+    return(<button onClick={(e) => handleClick(e)} style={styles}>Click Me 😁</button>);
 }
 
 export default Button;
